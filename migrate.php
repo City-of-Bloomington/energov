e46d2ff2a476be4ce6cfe025c8c05f5048692722
Cliff Ingham <inghamn@bloomington.in.gov>
2022-03-24 09:27:31 -0400

    Do not migrate attachments yet
    Commented out the attachment migration.  The plan is to pull attachments into EnerGov after we go live.
    
    Updates #24

--- migrate.php
+++ migrate.php
@@ -62,7 +62,7 @@ include './rental/permit_note.php';
 include './rental/permit_activity.php';
 include './rental/permit_fee.php';
 include './rental/payment.php';
-include './rental/attachment_document.php';
+// include './rental/attachment_document.php';
 
 include './row/contact.php';
 include './row/bond.php';
@@ -73,7 +73,7 @@ include './citation/code_case.php';
 include './citation/code_case_activity.php';
 include './citation/contact.php';
 include './citation/payment.php';
-include './citation/attachment_document.php';
+// include './citation/attachment_document.php';
 
 include './nov/contact.php';
 include './nov/code_case.php';
