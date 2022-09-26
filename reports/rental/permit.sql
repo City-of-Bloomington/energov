61bf78bcbbc60f15aeafb7b54c4264630fe28586
Cliff Ingham <inghamn@bloomington.in.gov>
2022-09-26 14:43:48 -0400

    Report: rental permit - added dates and inspector
    Updates #45

--- reports/rental/permit.sql
+++ reports/rental/permit.sql
@@ -1,48 +1,63 @@
 select permit.PMPERMITID,
        permit.PERMITNUMBER,
-       permit.ISSUEDATE,
-       permit.EXPIREDATE,
+       format(permit.ISSUEDATE,          'd') as issuedate,
+       format(permit.EXPIREDATE,         'd') as expiredate,
+       format(permit.lastinspectiondate, 'd') as lastinspectiondate,
+       concat_ws(' ', u.fname, u.lname)       as inspector,
 permitaddress.ADDRESSLINE1 as permit_address,
- owneraddress.ADDRESSLINE1 as owner_address,
- owneraddress.CITY         as owner_city,
- owneraddress.STATE        as owner_state,
- owneraddress.POSTALCODE   as owner_zip,
- agentaddress.ADDRESSLINE1 as agent_address,
- agentaddress.CITY         as agent_city,
- agentaddress.STATE        as agent_state,
- agentaddress.POSTALCODE   as agent_zip
-from dbo.PMPERMIT permit
-join dbo.CUSTOMSAVERPERMITMANAGEMENT fields on permit.PMPERMITID=fields.ID
-join dbo.PMPERMITADDRESS                pla on permit.PMPERMITID=pla.PMPERMITID
-join dbo.MAILINGADDRESS       permitaddress on pla.MAILINGADDRESSID=permitaddress.MAILINGADDRESSID
+ owner.name         as owner_name,
+ owner.ADDRESSLINE1 as owner_address,
+ owner.CITY         as owner_city,
+ owner.STATE        as owner_state,
+ owner.POSTALCODE   as owner_zip,
+ agent.name         as agent_name,
+ agent.ADDRESSLINE1 as agent_address,
+ agent.CITY         as agent_city,
+ agent.STATE        as agent_state,
+ agent.POSTALCODE   as agent_zip
+from PMPERMIT permit
+join CUSTOMSAVERPERMITMANAGEMENT fields on permit.PMPERMITID=fields.ID
+join PMPERMITADDRESS                pla on permit.PMPERMITID=pla.PMPERMITID
+join MAILINGADDRESS       permitaddress on pla.MAILINGADDRESSID=permitaddress.MAILINGADDRESSID
+join iminspectioncase                ic on permit.pmpermitid=ic.linkid
+join iminspection                     i  on i.iminspectioncaseid=ic.iminspectioncaseid and i.actualenddate=(select max(x.actualenddate) from iminspection x where x.iminspectioncaseid=ic.iminspectioncaseid)
+join iminspectorref ir on ir.inspectionid=i.iminspectionid and ir.bprimary=1
+join users u on ir.userid =u.suserguid
 left join (
-	select oc.PMPERMITID,
-	       oa.ADDRESSLINE1,
-		   oa.CITY,
-		   oa.STATE,
-		   oa.POSTALCODE
-	from dbo.PMPERMITCONTACT            oc
-	join dbo.LANDMANAGEMENTCONTACTTYPE  ot on oc.LANDMANAGEMENTCONTACTTYPEID=ot.LANDMANAGEMENTCONTACTTYPEID
-	join dbo.GLOBALENTITY               oe on oc.GLOBALENTITYID=oe.GLOBALENTITYID
-	join dbo.GLOBALENTITYMAILINGADDRESS om on oe.GLOBALENTITYID=om.GLOBALENTITYID
-	join dbo.MAILINGADDRESS             oa on om.MAILINGADDRESSID=oa.MAILINGADDRESSID
-	where ot.NAME='Owner'
-) owneraddress on permit.PMPERMITID=owneraddress.PMPERMITID
+    select oc.PMPERMITID,
+           case oe.iscompany when 1
+            then oe.globalentityname
+            else concat_ws(' ', oe.firstname, oe.middlename, oe.lastname)
+           end as name,
+           oa.ADDRESSLINE1,
+           oa.CITY,
+           oa.STATE,
+           oa.POSTALCODE
+    from PMPERMITCONTACT            oc
+    join LANDMANAGEMENTCONTACTTYPE  ot on oc.LANDMANAGEMENTCONTACTTYPEID=ot.LANDMANAGEMENTCONTACTTYPEID
+    join GLOBALENTITY               oe on oc.GLOBALENTITYID=oe.GLOBALENTITYID
+    join GLOBALENTITYMAILINGADDRESS om on oe.GLOBALENTITYID=om.GLOBALENTITYID
+    join MAILINGADDRESS             oa on om.MAILINGADDRESSID=oa.MAILINGADDRESSID
+    where ot.NAME='Owner'
+) owner on permit.PMPERMITID=owner.PMPERMITID
 left join (
-	select ac.PMPERMITID,
-	       aa.ADDRESSLINE1,
-		   aa.CITY,
-		   aa.STATE,
-		   aa.POSTALCODE
-	from dbo.PMPERMITCONTACT            ac
-	join dbo.LANDMANAGEMENTCONTACTTYPE  at on ac.LANDMANAGEMENTCONTACTTYPEID=at.LANDMANAGEMENTCONTACTTYPEID
-	join dbo.GLOBALENTITY               ae on ac.GLOBALENTITYID=ae.GLOBALENTITYID
-	join dbo.GLOBALENTITYMAILINGADDRESS am on ae.GLOBALENTITYID=am.GLOBALENTITYID
-	join dbo.MAILINGADDRESS             aa on am.MAILINGADDRESSID=aa.MAILINGADDRESSID
-	where at.NAME='Agent'
-) agentaddress on permit.PMPERMITID=agentaddress.PMPERMITID
-where permit.PMPERMITID='7450d8ba-dd77-4799-97fb-023e9b0684c0';
-
+    select ac.PMPERMITID,
+           case ae.iscompany when 1
+            then ae.globalentityname
+            else concat_ws(' ', ae.firstname, ae.middlename, ae.lastname)
+           end as name,
+           aa.ADDRESSLINE1,
+           aa.CITY,
+           aa.STATE,
+           aa.POSTALCODE
+    from PMPERMITCONTACT            ac
+    join LANDMANAGEMENTCONTACTTYPE  at on ac.LANDMANAGEMENTCONTACTTYPEID=at.LANDMANAGEMENTCONTACTTYPEID
+    join GLOBALENTITY               ae on ac.GLOBALENTITYID=ae.GLOBALENTITYID
+    join GLOBALENTITYMAILINGADDRESS am on ae.GLOBALENTITYID=am.GLOBALENTITYID
+    join MAILINGADDRESS             aa on am.MAILINGADDRESSID=aa.MAILINGADDRESSID
+    where at.NAME='Representative'
+) agent on permit.PMPERMITID=agent.PMPERMITID
+where permit.PMPERMITID='{?@PMPERMITID}';
 
 select min(case displayname when 'Structure Identifier' then value end) as structure,
        min(case displayname when 'Units'                then value end) as units,
